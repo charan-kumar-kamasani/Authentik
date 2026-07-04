ef452b56f9b51070f19b79fd058dee2b1ad4cfb8
charan-kumar-kamasani <charankumarreddy03@gmail.com>
2026-07-04 19:37:47 +0530

    moved aditional details to above to certificates

--- backend/test-phonepe.js
+++
@@ -1,8 +0,0 @@
-const { StandardCheckoutClient, Env } = require('pg-sdk-node');
-const { StandardCheckoutPayRequest } = require('pg-sdk-node');
-const request = StandardCheckoutPayRequest.builder()
-    .merchantOrderId("TEST123456")
-    .amount(100)
-    .redirectUrl("https://localhost")
-    .build();
-console.log(JSON.stringify(request, null, 2));
