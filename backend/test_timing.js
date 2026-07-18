443d483e1324bd91dd2df8bd422b630ea059fdae
charan-kumar-kamasani <charankumarreddy03@gmail.com>
2026-07-18 20:17:27 +0530

    updated prices

--- backend/test_timing.js
+++
@@ -1,9 +0,0 @@
-const { scrapeProductPrice } = require('./src/utils/scraper');
-
-const main = async () => {
-  const url = "https://www.flipkart.com/kenstar-black-pureza-8-l-ro-uv-uf-copper-alkaline-minerals-water-purifier-needs-no-service-2-years/p/itm123f0d1974f36";
-  console.time("⏱️ Flipkart Fetch Time");
-  await scrapeProductPrice(url);
-  console.timeEnd("⏱️ Flipkart Fetch Time");
-};
-main();
