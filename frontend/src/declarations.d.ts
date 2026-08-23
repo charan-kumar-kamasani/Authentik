dcda56d5817d210956bdb9b2ec1ccf5019288927
charan-kumar-kamasani <charankumarreddy03@gmail.com>
2026-08-24 00:37:53 +0530

    updated code

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 declare module "*.png" {
   const value: string;
   export default value;
@@ -11,6 +13,14 @@ declare module "*.jpeg" {
   export default value;
 }
 declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
+declare module "*.gif" {
   const value: string;
   export default value;
 }
